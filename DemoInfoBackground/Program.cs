98a2050d329fd1b36d13823db491aff4284e13ed
DmitrijNezh <dmitrij.nezh@gmail.com>
2016-01-18 23:41:11 +0200

    project init

---
+++ DemoInfoBackground/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoInfoBackground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HALO!");
+
+            Console.ReadKey();
+        }
+    }
+}
